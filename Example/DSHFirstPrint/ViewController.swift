cd507394dc84c2d88eef37bf7630e023a7453586
Dzmitry Shabailau <budulayreturns@gmail.com>
2020-12-13 18:33:44 +0300

    Added printFirst function

--- Example/DSHFirstPrint/ViewController.swift
+++ Example/DSHFirstPrint/ViewController.swift
@@ -7,18 +7,14 @@
 //
 
 import UIKit
+import DSHFirstPrint
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        printFirst()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
